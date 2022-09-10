f62c7e10558ca90e4b786510eb78c7f4c6005f9d
artur <artur@DESKTOP-V0JU5PP>
2022-09-10 16:43:55 -0300

    alteraçoes de menus

--- ProjetoOnTheFly/Program.cs
+++ ProjetoOnTheFly/Program.cs
@@ -10,6 +10,7 @@ namespace ProjetoOnTheFly
 
             MostrarMenuInicial();
         }
+
         static void MostrarMenuInicial()
         {
             string opcao;
@@ -18,13 +19,62 @@ namespace ProjetoOnTheFly
                 Console.Clear();
                 Console.WriteLine("°°°  MENU  INICIAL  °°°");
                 Console.WriteLine("opção 0 : sair");
+                Console.WriteLine("opção 1 : menu cadastro");
+                Console.WriteLine("opção 2 : menu localizar");
+                Console.WriteLine("opção 3 : menu editar");
+               
+                Console.Write("\n\tInforme a opcao: ");
+                opcao = Console.ReadLine();
+
+                if (opcao != "0" && opcao != "1" && opcao != "3")
+                {
+                    Console.WriteLine("'" + opcao + "' é uma opcao INVALIDA! Para voltar ao MENU, pressione QUALQUER TECLA!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                else
+                {
+                    switch (opcao)
+                    {
+                        case "0":
+                            Environment.Exit(0);
+                            break;
+
+                        case "1":
+                            Console.Clear();
+                            MostrarMenuCadastros();
+                            break;
+
+                        case "2":
+                            Console.Clear();
+                            MostrarMenuLocalizar();
+                            break;
+
+                        case "3":
+                            Console.Clear();
+                            MostrarMenuEditar();
+                            break;
+                    }
+                }
+            } while (true);
+        }
+
+        static void MostrarMenuCadastros()
+        {
+            string opcao;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("°°°  MENU  CADASTRO  °°°");
+                Console.WriteLine("opção 0 : sair");
                 Console.WriteLine("opção 1 : cadastrar passaageiro");
                 Console.WriteLine("opção 2 : cadastrar companhia aerea");
                 Console.WriteLine("opção 4 : cadastrar aeronave");
-                Console.WriteLine("opção 5 : bloqueados");
-                Console.WriteLine("opção 6 : registro de voo");
-                Console.WriteLine("opção 7 : registro de passagem");
-                Console.WriteLine("opção 8 : venda de passagem");
+                Console.WriteLine("opção 5 : cadstrar bloqueados");
+                Console.WriteLine("opção 6 : cadastro de voo");
+                Console.WriteLine("opção 7 : cadastro de passagem");
+                Console.WriteLine("opção 8 : cadastrar venda de passagem");
 
                 Console.Write("\n\tInforme a opcao: ");
                 opcao = Console.ReadLine();
@@ -89,6 +139,143 @@ namespace ProjetoOnTheFly
             } while (true);
         }
 
+        static void MostrarMenuLocalizar()
+        {
+            string opcao;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("°°°  MENU  LOCALIZAR  °°°");
+                Console.WriteLine("opção 0 : sair");
+                Console.WriteLine("opção 1 : localizar passaageiro");
+                Console.WriteLine("opção 2 : localizar companhia aerea");
+                Console.WriteLine("opção 4 : localizar aeronave");
+                Console.WriteLine("opção 5 : localizar bloqueados");
+                Console.WriteLine("opção 6 : localizar de voo");
+                Console.WriteLine("opção 7 : localizar de passagem");
+                Console.WriteLine("opção 8 : localizar venda de passagem");
 
+                Console.Write("\n\tInforme a opcao: ");
+                opcao = Console.ReadLine();
+
+                if (opcao != "0" && opcao != "1" && opcao != "3" && opcao != "4" && opcao != "5" && opcao != "6" && opcao != "7" && opcao != "8")
+                {
+                    Console.WriteLine("'" + opcao + "' é uma opcao INVALIDA! Para voltar ao MENU, pressione QUALQUER TECLA!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                else
+                {
+                    switch (opcao)
+                    {
+                        case "0":
+                            Environment.Exit(0);
+                            break;
+
+                        case "1":
+                            Console.Clear();
+
+                            break;
+
+                        case "2":
+                            Console.Clear();
+
+                            break;
+
+                        case "3":
+                            Console.Clear();
+
+                            break;
+
+                        case "4":
+                            Console.Clear();
+                            
+                            break;
+
+                        case "5":
+                            Console.Clear();
+
+                            break;
+
+                        case "6":
+                            Console.Clear();
+
+                            break;
+
+                        case "7":
+                            Console.Clear();
+
+                            break;
+
+                        case "8":
+                            Console.Clear();
+
+                            break;
+                    }
+                }
+            } while (true);
+        }
+
+        static void MostrarMenuEditar()
+        {
+                string opcao;
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("°°°  MENU  EDITAR  °°°");
+                    Console.WriteLine("opção 0 : sair");
+                    Console.WriteLine("opção 1 : editar passageiro");
+                    Console.WriteLine("opção 2 : editar companhia aerea");
+                    Console.WriteLine("opção 3 : editar aeronave");
+                    Console.WriteLine("opção 4 : editar voo");
+                    Console.WriteLine("opção 5 : editar passagem");
+
+                    Console.Write("\n\tInforme a opcao: ");
+                    opcao = Console.ReadLine();
+
+                    if (opcao != "0" && opcao != "1" && opcao != "3" && opcao != "4" && opcao != "5")
+                    {
+                        Console.WriteLine("'" + opcao + "' é uma opcao INVALIDA! Para voltar ao MENU, pressione QUALQUER TECLA!");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+
+                    else
+                    {
+                        switch (opcao)
+                        {
+                            case "0":
+                                Environment.Exit(0);
+                                break;
+
+                            case "1":
+                                Console.Clear();
+
+                                break;
+
+                            case "2":
+                                Console.Clear();
+
+                                break;
+
+                            case "3":
+                                Console.Clear();
+
+                                break;
+
+                            case "4":
+                                Console.Clear();
+
+                                break;
+
+                            case "5":
+                                Console.Clear();
+
+                                break;
+                        }
+                    }
+                } while (true);
+            }
     }
 }
