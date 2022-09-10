ba26d7fc0bc9c41f551430aeccf7004792ea2f33
WeslenAlmeida <weslen1202@gmail.com>
2022-09-10 07:43:20 -0300

    Inserindo Classes

---
+++ ProjetoOnTheFly/Passageiro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Passageiro
+    {
+        public string Nome { get; set; }
+        public string DataNascimento { get; set; }
+        public char Sexo { get; set; }
+        public string DataCadastro { get; set; }
+        public char Situacao { get; set; }
+
+    }
+}
